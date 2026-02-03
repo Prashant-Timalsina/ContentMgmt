4ec2acaf639ddeda6e57c3018c542e92ef995617
Prashant Timalsina <prashanttimalsina2@gmail.com>
2026-02-03 05:39:18 -0800

    Permission Guard + Roles and Permission seeders

--- backend/database/seeders/RoleSeeder.php
+++ backend/database/seeders/RoleSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;          
+use Spatie\Permission\Models\Permission;    
+use Spatie\Permission\PermissionRegistrar;   
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +15,22 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Created Permission and assigned roles with permissions
+        app() [PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'create articles']);
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'view articles']);
+        Permission::create(['name' => 'delete articles']);
+
+        $admin = Role::create(['name' => 'admin']);
+        $admin->givePermissionTo(Permission::all());
+
+        $editor = Role::create(['name' => 'editor']);
+        $editor->givePermissionTo(['edit articles','create articles','view articles']);
+
+        $user = Role::create(['name' => 'user']);
+        $user->givePermissionTo(['view articles']);
+        
     }
 }
