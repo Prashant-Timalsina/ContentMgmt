12bd2f5ae493138f0773a1b882c463c2bcc7140b
Prashant Timalsina <prashanttimalsina2@gmail.com>
2026-02-04 01:10:07 -0800

    register, login routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\UserController;
+
+Route::post('/login',[UserController::class])->name('user.login');
+Route::post('/register',[UserController::class])->name('user.register');
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('me', function (Request $request){
+        return $request->user();
+    });
+});
