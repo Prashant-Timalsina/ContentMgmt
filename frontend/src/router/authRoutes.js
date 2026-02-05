61560494bb12d3a1d01fe90e677b1ef5eaacb3cb
Prashant Timalsina <prashanttimalsina2@gmail.com>
2026-02-05 04:00:57 -0800

    cors setup

---
+++ frontend/src/router/authRoutes.js
@@ -0,0 +1,10 @@
+// import { defineStore } from "pinia";
+
+// const authStore = defineStore('auth', {
+//     state :() => ({
+//         user:[],
+//         token:'',
+//         loading:false
+//     }),
+    
+// })
